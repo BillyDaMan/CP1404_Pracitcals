93c4614074086a321b74a3585b75905320b4b91b
Billy <billy.domanti@my.jcu.edu.au>
2022-10-02 18:25:40 +1000

    Randoms

---
+++ prac_03/randoms.py
@@ -0,0 +1,9 @@
+"""
+randoms.py
+
+Line 1: 10, the lowest possible number is 5, the highest possible number is 20
+Line 2: 5, the lowest possible number is 3, the highest possible number is 9
+Line 3: 2.9527496987944954, the lowest possible number is 2.5, the highest possible number is 5.5
+"""
+import random
+print(random.randint(1, 100))
