ff4de9e3dc4c3e4472f4d2c383be26109a5a58a0
Billy <billy.domanti@my.jcu.edu.au>
2022-10-19 22:02:38 +1000

    Displays how many times a word occurred in a sentence

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,18 @@
+"""
+Word Occurrences
+Estimate: 30 minutes
+Actual:   30 minutes
+"""
+
+word_to_count = {}
+text = input("Text: ")
+words = text.split()
+for word in words:
+    recurrence = word_to_count.get(word, 0)
+    word_to_count[word] = recurrence + 1
+words = list(word_to_count.keys())
+words.sort()
+
+max_length = max((len(word) for word in words))
+for word in words:
+    print(f"{word:{max_length}} : {word_to_count[word]}")
